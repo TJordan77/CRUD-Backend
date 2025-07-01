eaf50150ef9e6b1d1beeedbc7031b9b5e4304e9b
Tran Vo <vobaohoangtran@gmail.com>
2025-07-01 15:26:43 -0400

    feat(models): Added Student Model/missing gpa field + export

--- database/Student.js
+++ database/Student.js
@@ -1,1 +1,32 @@
+const { DataTypes, Model } = require("sequelize");
+const db = require("./db");
 
+const Student = db.define("student", {
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+            notEmpty: true,
+        },
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        defaultValue: "https://enter.your.url/here.jpg",
+    },
+    }
+);
