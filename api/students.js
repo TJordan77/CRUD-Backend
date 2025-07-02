a62d0eb07bec4f3d16aa035f8802ac4bae0a83ab
TJordan77 <tvon777@gmail.com>
2025-07-02 15:29:57 -0400

    Update students.js

--- api/students.js
+++ api/students.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   try {
     const student = await Student.findbyPK(req.params.id, {include: Campus});
-    Student ? res.json(student): res.sendStatus(404);  //Shortcut if/else statement lol
+    student ? res.json(student): res.sendStatus(404);  //Shortcut if/else statement lol
   } catch (err) {
     next(err);
   }
