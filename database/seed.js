1cb720f73ee751e3e8e3a642703b678d6700659c
Tran Vo <vobaohoangtran@gmail.com>
2025-07-02 14:03:48 -0400

    feat(databse): update seed file for campus and students

--- database/seed.js
+++ database/seed.js
@@ -1,5 +1,6 @@
 const db = require("./db");
 const { Duck } = require("./index");
+const { Campus, Student } = require("./index");
 
 const seed = async () => {
   db.logging = false;
@@ -9,8 +10,10 @@ const seed = async () => {
     { name: "Main Building", address: "199 Chambers St, New York, NY 10007", imageUrl: "https://placehold.co/200x200", description: "Main Building of Borough of Manhattan Community College." },
   ]);
   const students = await Student.bulkCreate([
-    { firstName: "Allan", lastName: "Smith", email: "alice@example.com", campusId: campuses[0].id },
-    { firstName: "Finn", lastName: "Jones", email: "bob@example.com", campusId: campuses[1].id },
+    { firstName: "Allan", lastName: "James Lapid", email: "alice@example.com", campusId: campuses[0].id },
+    { firstName: "Finn", lastName: "Instructor", email: "bob@example.com", campusId: campuses[1].id },
   ]);
-
+console.log("Seeded database with campuses");
+db.close();
+};
 seed();
