2d8bd8ab2fadf1e51509ea917cf9b4ae55aaab1f
Tran Vo <vobaohoangtran@gmail.com>
2025-07-02 11:01:45 -0400

    fix(apis):fixed get all campus routes and minor fixes for single campus/ for front end connection

--- api/campuses.js
+++ api/campuses.js
@@ -6,7 +6,8 @@ const {Campus, Student} = require("../database");
 router.get("/", async (req, res, next) => {
     try {
         const campuses = await Campus.findAll({include: Student});
-        Campus ? res.json(campuses): res.sendStatus(404); //Shortcut if/else statement lol
+        //Campus ? res.json(campuses): res.sendStatus(404); //Shortcut if/else statement lol
+        res.json(campuses);
     } catch (err) {
       next(err);
     }
@@ -16,8 +17,12 @@ router.get("/", async (req, res, next) => {
 // Single Campus
 router.get("/:id", async (req, res, next) => {
   try {
-    const campus = await Campus.findbyPK(req.params.id, {include: Student, });
-    res.json(campus);
+    const campus = await Campus.findByPk(req.params.id, {include: Student, }); //minor fix: findbyPK
+    if (campus) {
+      res.json(campus);
+    } else {
+      res.sendStatus(404);
+    }
   } catch (err) {
     next(err);
   }
