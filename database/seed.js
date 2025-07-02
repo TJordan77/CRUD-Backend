4aaa88658873b4020a25205c7135c44a60029566
Tran Vo <vobaohoangtran@gmail.com>
2025-07-02 13:59:48 -0400

    feat(database):Seed database with Fitterman Hall, Main Building of BMCC, and students

--- database/seed.js
+++ database/seed.js
@@ -4,16 +4,13 @@ const { Duck } = require("./index");
 const seed = async () => {
   db.logging = false;
   await db.sync({ force: true }); // Drop and recreate tables
-  const ducks = await Duck.bulkCreate([
-    { name: "James Pond" },
-    { name: "Quakie Chan" },
-    { name: "Goose" },
+  const campuses = await Campus.bulkCreate([
+    { name: "Fitterman Hall", address: "245 Greenwich St, New York, NY 10007", imageUrl: "https://placehold.co/200x200", description: "Fitterman Hall at BMCC." },
+    { name: "Main Building", address: "199 Chambers St, New York, NY 10007", imageUrl: "https://placehold.co/200x200", description: "Main Building of Borough of Manhattan Community College." },
   ]);
-
-  console.log(`🦆 Created ${ducks.length} ducks`);
-
-  console.log("🌱 Seeded the database");
-  db.close();
-};
+  const students = await Student.bulkCreate([
+    { firstName: "Allan", lastName: "Smith", email: "alice@example.com", campusId: campuses[0].id },
+    { firstName: "Finn", lastName: "Jones", email: "bob@example.com", campusId: campuses[1].id },
+  ]);
 
 seed();
