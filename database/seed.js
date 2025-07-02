c9cb74aa49e4b48dd969545288583a1693005b9d
Tran Vo <vobaohoangtran@gmail.com>
2025-07-02 15:56:23 -0400

    fix(database): added gpa data and code clean up

--- database/seed.js
+++ database/seed.js
@@ -5,12 +5,31 @@ const seed = async () => {
   db.logging = false;
   await db.sync({ force: true }); // Drop and recreate tables
   const campuses = await Campus.bulkCreate([
-    { name: "Fitterman Hall", address: "245 Greenwich St, New York, NY 10007", imageUrl: "https://placehold.co/200x200", description: "Fitterman Hall at BMCC." },
-    { name: "Main Building", address: "199 Chambers St, New York, NY 10007", imageUrl: "https://placehold.co/200x200", description: "Main Building of Borough of Manhattan Community College." },
+    { name: "Fitterman Hall",
+      address: "245 Greenwich St, New York, NY 10007", 
+      imageUrl: "https://placehold.co/200x200", 
+      description: "Fitterman Hall at BMCC."
+     },
+    { name: "Main Building", address: "199 Chambers St, New York, NY 10007", 
+      imageUrl: "https://placehold.co/200x200", 
+      description: "Main Building of Borough of Manhattan Community College." 
+    },
   ]);
   const students = await Student.bulkCreate([
-    { firstName: "Allan", lastName: "James Lapid", email: "allan@example.com", campusId: campuses[0].id },
-    { firstName: "Finn", lastName: "Instructor", email: "finn@example.com", campusId: campuses[1].id },
+    { 
+      firstName: "Allan", 
+      lastName: "James Lapid", 
+      email: "allan@example.com", 
+      gpa: 3.8,
+      campusId: campuses[0].id 
+    },
+    { 
+      firstName: "Finn", 
+      lastName: "Instructor", 
+      email: "finn@example.com",
+      gpa: 3.9, 
+      campusId: campuses[1].id 
+    },
   ]);
 console.log("Seeded database with campuses");
 db.close();
