81d96617a2ec4e5edd254186852c0bfb2f872153
TJordan77 <tvon777@gmail.com>
2025-07-03 09:51:45 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
+require("dotenv").config(); 
+
 // Feel free to rename the database to whatever you want!
 const dbName = "ttp_crud";
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
   {
-    logging: false, // comment this line to enable logging
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
   }
 );
 
